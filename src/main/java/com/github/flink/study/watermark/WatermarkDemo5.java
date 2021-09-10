7100dfa525489bf333dfa5a05c8526bd9d68bedb
Gavin <gavin_chow@outlook.com>
2021-09-10 16:17:09 +0800

    try to read sourcecode about WindowAssigner.

---
+++ src/main/java/com/github/flink/study/watermark/WatermarkDemo5.java
@@ -0,0 +1,34 @@
+package com.github.flink.study.watermark;
+
+import com.github.flink.study.common.UserEvent;
+import com.github.flink.study.source.WatermarkDemoSource;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.WindowedStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+
+import java.time.Duration;
+
+public class WatermarkDemo5
+{
+    public static void main(String[] args)
+            throws Exception
+    {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStream<UserEvent> source = env
+                .addSource(new WatermarkDemoSource())
+                .name("watermark-test");
+
+        WindowedStream<UserEvent, String, TimeWindow> window = source
+                .assignTimestampsAndWatermarks(WatermarkStrategy.<UserEvent>forBoundedOutOfOrderness(Duration.ofSeconds(3L))
+                        .withTimestampAssigner((SerializableTimestampAssigner<UserEvent>) (element, recordTimestamp) -> element.getEventTime()))
+                .keyBy(UserEvent::getUserId)
+                .window(TumblingEventTimeWindows.of(Time.seconds(3L)));
+
+        env.execute("kafka source demo");
+    }
+}
