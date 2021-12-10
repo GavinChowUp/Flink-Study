c6390c1184cb83d5967fcdb3db05fd5de6236e3d
Gavin <gavin_chow@outlook.com>
2021-12-10 19:14:07 +0800

    Add JMH.

---
+++ src/main/java/com/github/flink/study/Quick.java
@@ -0,0 +1,16 @@
+package com.github.flink.study;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Quick
+{
+    public static void main(String[] args)
+    {
+        List<String> strings = Arrays.asList(null, null);
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+}
---
+++ src/main/java/com/github/flink/study/topn/ForCartesian.java
@@ -0,0 +1,5 @@
+package com.github.flink.study.topn;
+
+public class ForCartesian
+{
+}
